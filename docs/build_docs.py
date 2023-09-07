1b9bc572149496f70c4b843283909f90615e695c
Thomas <thomas.sedlmair@yunextraffic.com>
2023-09-07 17:30:43 +0200

    updated example

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -19,7 +19,7 @@ def move_dir(src, dst):
 # just to not always run the entire build we push a build_all_docs
 os.environ["build_all_docs"] = str(True)
 # and the root path where the docs are available
-os.environ["pages_root"] = "https://its.pages.mocca.yunextraffic.cloud/ta/vscode-yunextest" 
+os.environ["pages_root"] = "https://those1990.github.io/SphinxExample" 
 
 # we treat the main branch a bit different because we want to have english latest as landing page
 build_doc("latest", "en", "main")
